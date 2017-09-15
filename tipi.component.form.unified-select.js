2c28b0f6bb866c7e10faf823727fdd74741fa9d2
Thomas van der Velde <contact@toolbarthomas.net>
2017-09-15 11:15:20 +0200

    Filter non native select box

--- tipi.component.form.unified-select.js
+++ tipi.component.form.unified-select.js
@@ -7,9 +7,10 @@
 			setUnifiedSelectDisabledState(unified_select);
 			setUnifiedSelectEvents(unified_select);
 			setUnifiedSelectValue(unified_select);
-
+			setUnifiedSelectNativeState(unified_select);
 			unsetUnifiedSelectDropdown(unified_select);
 			setUnifiedSelectDropdown(unified_select);
+			setUnifiedSelectFilter(unified_select);
 			setUnifiedSelectDropdownItemEvents(unified_select);
 
 			setUnifiedSelectDropdownEvents(unified_select);
@@ -48,11 +49,13 @@
 			$(window).trigger('tipi.unified-select.init', [unified_select]);
 		});
 	}
-
+	
+	// Add the ready state and show it to the world
 	function setUnifiedSelectReadyState(unified_select) {
 		unified_select.addClass('js__select--ready');		
 	}
 
+	// Add the disabled state if our select is disabled
 	function setUnifiedSelectDisabledState(unified_select) {
 		var disabled = getUnifiedSelectDisabledState(unified_select);
 
@@ -94,21 +97,18 @@
 			},
 			change: function () {
 				$(document).trigger('tipi.unified-select.setValue', [unified_select]);
-				console.log('update');
 			}
 		});
 	}
 
 	// Append the dropdown and items for our select
 	function setUnifiedSelectValue(unified_select) {
-		var unified_select_value = unified_select.find('.select-value');
+		var unified_select_field_value = unified_select.find('.select-field-value');
 
 		var select = unified_select.find('select');
 
 		var selected_index = select.get(0).selectedIndex;
 		var option = select.find('option').eq(selected_index);
-
-		console.log(selected_index);
 
 		if (option.length === 0) {
 			return;
@@ -117,10 +117,42 @@
 		var label = getUnifiedSelectOptionLabelAttribute(option);
 		
 		// Set the actual selected value!
-		unified_select_value.html(label);
+		unified_select_field_value.html(label);
+	}
+
+	// Setup our unified select so it uses the native browser controls
+	function setUnifiedSelectNativeState(unified_select) {
+		if(!unified_select.hasClass('select--native')) {
+			unsetUnifiedSelectNativeState(unified_select);
+			return;
+		}
+
+		// Make sure our unified select has no tab-index so we can focus the select directly
+		unified_select.removeAttr('tabindex');
+
+		// Remove any events we've added in the native state
+		unified_select.off('focus blur');
+	}
+
+	function unsetUnifiedSelectNativeState(unified_select) {
+		// Enable support for tabbing the selectbox
+		unified_select.attr('tabindex', '0');
+
+		unified_select.on({
+			focus: function () {
+				unified_select.addClass('js__select--focus');
+			},
+			blur: function () {
+				unified_select.removeClass('js__select--focus');
+			}
+		});
+
+		var select = unified_select.find('select');
+		// Disable support for tabbing in the actual select element
+		select.attr('tabindex', '-1');
+		
 	}
 	
-
 	// Remove the dropdown and items for our select
 	function unsetUnifiedSelectDropdown(unified_select) {
 		var unified_select_dropdown = unified_select.find('.select-dropdown');
@@ -136,6 +168,11 @@
 
 	// Append the dropdown and items for our select
 	function setUnifiedSelectDropdown(unified_select) {
+		// Don't append the dropdown if we wan't to use the native select
+		if (unified_select.hasClass('select--native')) {
+			return;
+		}
+
 		var unified_select_dropdown = unified_select.find('.select-dropdown');
 		var unified_select_dropdown_items = unified_select_dropdown.find('.select-dropdown-items');
 		var unified_select_dropdown_item = unified_select_dropdown_items.find('.select-dropdown-item');
@@ -161,6 +198,8 @@
 		unified_select_dropdown.append('<div class="select-dropdown-items"></div>');
 		unified_select_dropdown_items = unified_select_dropdown.find('.select-dropdown-items');
 
+		var json_data = [];
+
 		// Loop through each select option and set it's attribute
 		options.each(function(index) {
 			var option = $(this);
@@ -173,8 +212,109 @@
 				disabled = '';
 			}
 
+			json_data.push(label);
+
 			unified_select_dropdown_items.append('<div class="select-dropdown-item' + disabled + '">' + label + '</div>');			
 		});
+
+		unified_select.data('json_data', json_data);
+	}
+
+	// Append the Filter input
+	function setUnifiedSelectFilter(unified_select) {
+		// Don't append the dropdown if we wan't to use the native select
+		if (unified_select.hasClass('select--native')) {
+			return;
+		}
+
+		// Cancel if we have no json data to filter
+		if (typeof unified_select.data('json_data') === 'undefined') {
+			return;
+		}
+
+		var unified_select_dropdown = unified_select.find('.select-dropdown');
+		if (unified_select_dropdown.length === 0) {
+			return;
+		}
+
+		unified_select_dropdown.prepend('<div class="select-filter"><input type="text" class="select-filter-input" /></div>');
+		var unified_select_filter_input = unified_select_dropdown.find('.select-filter-input');
+
+		var select = unified_select.find('select');
+		var name = select.attr('name');
+
+		if(typeof name != 'undefined' && name != '') {
+			unified_select_filter_input.attr('name', name + '-filter');
+		}
+
+		var timeout;
+
+		unified_select_filter_input.on({
+			'keyup keydown':function(event) {
+				if (event.keyCode == 13) {
+					event.preventDefault();
+					return false;
+				}
+
+				var filter = $(this);
+
+				clearTimeout(timeout);
+				timeout = setTimeout(function () {
+					filterUnifiedSelect(filter);
+
+				}, 100);
+			},
+			'keyup': function (event) {
+				event.preventDefault();
+				
+			}
+		});
+	}
+
+	// Filter the input
+	function filterUnifiedSelect(input) {
+		var unified_select = input.closest('.select');
+		if (unified_select.length === 0) {
+			return;
+		}
+
+		var json_data = unified_select.data('json_data');
+		if (typeof json_data === 'undefined') {
+			return;
+		}
+
+		if (json_data.length === 0) {
+			return;
+		}
+
+		var term = String(input.val());
+		var query = term.toUpperCase();
+
+		var found_indexes = [];
+
+		var results = $.grep(json_data, function (value, index) {
+			if (String(value).toUpperCase().indexOf(query) >= 0 ) {
+				found_indexes.push(index);
+				return;
+			}
+		});
+
+		console.log(found_indexes);
+
+		var unified_select_dropdown_item = unified_select.find('.select-dropdown-item');
+		//Hide all 
+		unified_select_dropdown_item.removeClass('js__select-dropdown-item--hide');
+
+		if(found_indexes.length === 0){
+			return;
+		}
+
+		// Remove the found items from the array
+		for (var i = found_indexes.length - 1; i >= 0; i--) {
+			unified_select_dropdown_item.splice(found_indexes[i], 1);
+		}
+
+		unified_select_dropdown_item.addClass('js__select-dropdown-item--hide');
 	}
 
 	// Define the label for our dropdown item
@@ -207,6 +347,11 @@
 
 	// Bind the event needed to change the select
 	function setUnifiedSelectDropdownItemEvents(unified_select) {
+		// Don't append the dropdown if we wan't to use the native select
+		if (unified_select.hasClass('select--native')) {
+			return;
+		}
+
 		var unified_select_dropdown_item = unified_select.find('.select-dropdown-item');
 		var select = unified_select.find('select');
 
@@ -216,8 +361,6 @@
 				
 				var item = $(this);
 				var index = item.index();
-
-				console.log(index);
 
 				// Fire the callback and update our select
 				$(document).trigger('tipi.unified-select.change', [unified_select, index]);
@@ -246,18 +389,23 @@
 	}
 
 	function setUnifiedSelectDropdownEvents(unified_select) {
-		var unified_select_value = unified_select.find('.select-value');
+		// Don't append the dropdown if we wan't to use the native select
+		if (unified_select.hasClass('select--native')) {
+			return;
+		}
 
-		unified_select_value.on({
+		var unified_select_field = unified_select.find('.select-field');
+
+		unified_select_field.on({
 			click : function(event) {
 				event.preventDefault();
 
-				var dropdown_is_open = unified_select.hasClass('js__select--dropdown-open');;
+				var dropdown_is_open = unified_select.hasClass('js__select--active');
 				if (dropdown_is_open) {
-					$(document).trigger('tipi.unified-select.open', [unified_select]);
+					$(document).trigger('tipi.unified-select.close', [unified_select]);
 				}
 				else {
-					$(document).trigger('tipi.unified-select.close', [unified_select]);
+					$(document).trigger('tipi.unified-select.open', [unified_select]);
 				}
 			}
 		})
@@ -269,11 +417,11 @@
 			return;
 		}
 
-		unified_select.addClass('js__select--dropdown-open')
+		unified_select.addClass('js__select--active')
 	}
 
 	function closeUnifiedSelectDropdown(unified_select) {
-		unified_select.removeClass('js__select--dropdown-open')
+		unified_select.removeClass('js__select--active')
 	}
 
 	// disabled	disabled	Specifies that an option should be disabled
